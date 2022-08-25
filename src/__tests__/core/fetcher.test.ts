e0fa6b9cce52b9ff66bca067c718b68b10314c4f
kazuya kawaguchi <kawakazu80@gmail.com>
2022-08-25 20:27:45 +0900

    implement fetcher

--- src/__tests__/core/fetcher.test.ts
+++ src/__tests__/core/fetcher.test.ts
@@ -1,19 +1,64 @@
+import { spawn } from 'node:child_process'
 import { fetchGithubRelease } from '../../fetcher'
+import release from '../fixtures/release.json'
+
+import type { ChildProcess } from 'node:child_process'
+import type { GitHubRelease, Fetcher } from '../../types'
+
+vi.mock('../../utils', () => {
+  return {
+    existCommand: vi.fn().mockImplementation(() => Promise.resolve(true))
+  }
+})
+
+vi.mock('node:child_process', () => {
+  return {
+    spawn: vi.fn()
+  }
+})
 
 describe('fetchGithubRelease', () => {
-  test.todo('default', async () => {
-    // TODO:
+  test('default', async () => {
+    // mocking
+    const on = vi.fn().mockImplementationOnce((event: string, cb: Function) => {
+      if (event === 'data') {
+        cb(JSON.stringify(release))
+      }
+    })
+    vi.mocked(spawn).mockImplementationOnce(() => {
+      return { stdout: { on } } as unknown as ChildProcess
+    })
+
+    // assertion
+    expect(await fetchGithubRelease(release.tagName)).toEqual(release)
   })
 
-  test.todo('fetcher plugin', async () => {
-    // TODO:
+  test('custom fetcher', async () => {
+    const releaseData: GitHubRelease = {
+      name: 'first release',
+      tagName: 'v0.0.0',
+      publishedAt: '2022-08-22T17:22:41Z',
+      url: 'https://github.com/your/repo/releases/tag/v0.0.0',
+      body: 'This is initial release'
+    }
+
+    // custom fetcher (e.g GitHub API)
+    const myFetcher: Fetcher = async (tag: string) => {
+      return new Promise(resolve => {
+        // api call ...
+        setTimeout(() => resolve(releaseData), 100)
+      })
+    }
+
+    // assertion
+    expect(await fetchGithubRelease(releaseData.tagName, myFetcher)).toEqual(releaseData)
   })
 
-  test.todo('not compatible fetcher plugin', async () => {
-    // TODO:
-  })
+  test('not compatible fetcher', async () => {
+    // not callable fetcher
+    const myFetcher: Fetcher = 'not callable' as unknown as Fetcher
 
-  test.todo('unexpteced error', async () => {
-    // TODO:
+    // assertions
+    await expect(fetchGithubRelease(release.tagName, myFetcher)).rejects.toThrow('fetcher is not a function')
   })
 })
--- src/fetcher.ts
+++ src/fetcher.ts
@@ -1,9 +1,30 @@
+import { spawn } from 'node:child_process'
+import { existCommand } from './utils'
+
 import type { GitHubRelease, Fetcher } from './types'
 
-function fetcher(tag: string): Promise<GitHubRelease> {
-  throw new Error('not implemented')
+async function fetcherDefault(tag: string): Promise<GitHubRelease> {
+  if (!(await existCommand('gh'))) {
+    throw new Error('not found gh command')
+  }
+
+  return new Promise((resolve, reject) => {
+    try {
+      const child = spawn('gh', ['release', 'view', tag, '--json', 'tagName,name,url,publishedAt,body'], {
+        env: { ...process.env }
+      })
+      child.stdout.on('data', data => {
+        resolve(JSON.parse(data) as GitHubRelease)
+      })
+    } catch (e) {
+      reject(e)
+    }
+  })
 }
 
-export async function fetchGithubRelease(tag: string): Promise<GitHubRelease> {
-  throw new Error('not implemented')
+export async function fetchGithubRelease(tag: string, fetcher: Fetcher = fetcherDefault): Promise<GitHubRelease> {
+  if (typeof fetcher !== 'function') {
+    throw new Error('fetcher is not a function')
+  }
+  return fetcher(tag)
 }
