fe937ccc6b5d0b8611e1d3e9b6b97c813006b8e5
kazuya kawaguchi <kawakazu80@gmail.com>
2022-08-25 19:27:27 +0900

    implement util

--- src/__tests__/core/utils.test.ts
+++ src/__tests__/core/utils.test.ts
@@ -1,19 +1,54 @@
+import { vi } from 'vitest'
 import { existCommand } from '../../utils'
+import { spawn } from 'node:child_process'
+
+import type { ChildProcess } from 'node:child_process'
+
+vi.mock('node:child_process', () => {
+  return {
+    spawn: vi.fn()
+  }
+})
 
 describe('existCommand', () => {
-  test.todo('found', async () => {
-    // TODO:
+  test('found', async () => {
+    // mocking
+    vi.mocked(spawn).mockImplementationOnce(() => {
+      return { exitCode: 0 } as ChildProcess
+    })
+
+    // assertions
+    expect(await existCommand('git')).toBe(true)
   })
 
-  test.todo('not found', async () => {
-    // TODO:
+  test('not found', async () => {
+    // mocking
+    vi.mocked(spawn).mockImplementationOnce(() => {
+      return { exitCode: 1 } as ChildProcess
+    })
+
+    // assertions
+    expect(await existCommand('git')).toBe(false)
   })
 
-  test.todo('found via "exit" event', async () => {
-    // TODO:
+  test('found via "exit" event', async () => {
+    // mocking
+    const on = vi.fn().mockImplementationOnce((_, cb: Function) => cb(0))
+    vi.mocked(spawn).mockImplementationOnce(() => {
+      return { exitCode: null, on } as unknown as ChildProcess
+    })
+
+    // assertions
+    expect(await existCommand('git')).toBe(true)
   })
 
-  test.todo('unexpteced error', async () => {
-    // TODO:
+  test('unexpteced error', async () => {
+    // mocking
+    vi.mocked(spawn).mockImplementationOnce(() => {
+      throw new Error('unexpteced error')
+    })
+
+    // assertions
+    await expect(existCommand('git')).rejects.toThrow()
   })
 })
--- src/utils.ts
+++ src/utils.ts
@@ -1,3 +1,16 @@
+import { spawn } from 'node:child_process'
+
 export function existCommand(command: string): Promise<boolean> {
-  throw new Error('not implemented')
+  return new Promise((resolve, reject) => {
+    try {
+      const child = spawn('which', [command])
+      if (child.exitCode !== null) {
+        resolve(child.exitCode === 0)
+      } else {
+        child.on('exit', code => resolve(code === 0))
+      }
+    } catch (e) {
+      reject(e)
+    }
+  })
 }
