ab69d4f5ac0a21b5fa42b7c29cbef7c965dad52b
kazuya kawaguchi <kawakazu80@gmail.com>
2022-09-04 18:24:11 +0900

    feat: output option

--- src/utils.ts
+++ src/utils.ts
@@ -1,4 +1,5 @@
 import { spawn } from 'node:child_process'
+import { constants as FS_CONSTANTS, promises as fs } from 'node:fs'
 
 export function existCommand(command: string): Promise<boolean> {
   return new Promise((resolve, reject) => {
@@ -18,4 +19,13 @@ export function existCommand(command: string): Promise<boolean> {
 // eslint-disable-next-line @typescript-eslint/ban-types
 export function isFunction(value: unknown): value is Function {
   return typeof value === 'function'
+}
+
+export async function isExists(path: string) {
+  try {
+    await fs.access(path, FS_CONSTANTS.F_OK)
+    return true
+  } catch {
+    return false
+  }
 }
